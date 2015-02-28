2e5f0e4ff267da4c17bea058458f66c665acf759
Stéphane Cottin <stephane.cottin@vixns.com>
2015-02-28 18:47:05 +0100

    remove bashinsm

--- dig-srv.sh
+++ dig-srv.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-IFS=$'\n'
+IFS=$'
+'
 dn="$1"
-for entry in $(dig +short srv $dn | awk '{print $NF}'); do
+for entry in $(dig +short srv $dn | awk '{print $NF}')
+do
   ip=$(dig srv +noanswer $dn | grep "$entry" | awk '{print $NF}')
   res="${res}${ip}:$(dig +short srv $dn | grep "$entry" | awk '{print $(NF-1)}'),";
 done
